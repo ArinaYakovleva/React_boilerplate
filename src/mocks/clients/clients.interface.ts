ace76375e00aa3fdce55a798759ce41a1bb173f5
Artem Evseev <a.ewseew@walli.com>
2020-10-13 10:42:57 +0300

    Mocked up clients data for future development

---
+++ src/mocks/clients/clients.interface.ts
@@ -0,0 +1,14 @@
+export interface Vehicle {
+    manufacturer: string;
+    model: string;
+    image?: string;
+    year: number;
+}
+
+export interface Client {
+    firstName: string;
+    lastName: string;
+    age: number;
+    phone?: string;
+    vehicles?: Vehicle[];
+}
---
+++ src/mocks/clients/index.ts
@@ -0,0 +1,35 @@
+import { Client } from "./clients.interface";
+
+export const MockClients: Client[] = [
+    {
+        firstName: "Андрей",
+        lastName: "Джээсов",
+        age: 31,
+        phone: "9674566666",
+        vehicles: [
+            {
+                manufacturer: "Toyota",
+                model: "Corolla",
+                year: 1998,
+                image: "https://1.bp.blogspot.com/-sz02N6PTVbc/TuNRP5DivQI/AAAAAAAAAGA/DeQATrouqII/s1600/98812041990002-480.jpg"
+            }
+        ]
+    },
+    {
+        firstName: "Борис",
+        lastName: "Баранов",
+        age: 20,
+    },
+    {
+        firstName: "Анжела",
+        lastName: "Ленинградская",
+        age: 23,
+        vehicles: [
+            {
+                manufacturer: "Mercedes",
+                model: "S Classe S500",
+                year: 2020
+            }
+        ]
+    }
+];
