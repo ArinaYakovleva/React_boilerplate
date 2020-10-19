1ef44b6643210017cb95b1a7f389e4f7a1f4ac0f
Arina <yakovlevaarischa@yandex.ru>
2020-10-19 15:47:31 +0300

    error fixed

--- src/actions/clients.ts
+++ src/actions/clients.ts
@@ -1,4 +1,5 @@
 export const CLIENTS_LOAD = 'CLIENTS_LOAD';
+export const SEND_DATA = 'SEND_DATA';
 
 type ClientsLoadActions = {
     type: 'CLIENTS_LOAD';
@@ -6,7 +7,11 @@ type ClientsLoadActions = {
 }
 
 
-export const clientsLoadActions = (clients: any): ClientsLoadActions => ({
+export const clientsLoadActions = (): ClientsLoadActions => ({
     type: CLIENTS_LOAD,
 });
+
+export const sendDataActions = () => {
+
+}
 
